47b0985085cb82712e430ec111b47bea4e3ae3e6
Surya Raman <ayrusme@gmail.com>
2017-11-14 19:47:41 +0530

    Added NO2 sub index calculation

--- parser.py
+++ parser.py
@@ -7,6 +7,7 @@ import pandas as pd
 def construct_initial_dataframe():
     """Function to read resources and construct the initial dataframe"""
     reqd_cols = ['Sampling Date', 'State', 'City/Town/Village/Area', 'Location of Monitoring Station', 'SO2', 'NO2', 'RSPM/PM10']
+    #Reading all files inside resources folder
     files_list = os.listdir('resources')
     df_list = []
     for filex in files_list:
@@ -14,10 +15,12 @@ def construct_initial_dataframe():
         df.fillna(0, inplace=True)
         df[['SO2', 'NO2', 'RSPM/PM10']] = df[['SO2', 'NO2', 'RSPM/PM10']].astype(numpy.int64)
         df_list.append(df)
+    #Sending back a list of dataframes
     return df_list
 
 def calculate_sub_index_pm10(pm_10):
     """Function to calculate the sub-index of the pollutant PM10"""
+    #Formula taken from AQI calculator excel sheet
     if pm_10 <= 100:
         return pm_10
     elif pm_10 > 100 and pm_10 <= 250:
@@ -31,6 +34,7 @@ def calculate_sub_index_pm10(pm_10):
 
 def calculate_sub_index_so2(so2):
     """Function to calculate the sub-index of the pollutant SO2"""
+    #Formula taken from AQI calculator excel sheet
     if so2 <= 40:
         return so2*50/40
     elif so2 > 40 and so2 <=80:
@@ -44,7 +48,25 @@ def calculate_sub_index_so2(so2):
     elif so2 > 1600:
         return 400 + ( so2 - 1600) * 100/80
 
+def calculate_sub_index_no2(no2):
+    """Function to calculate the sub-index of the pollutant no2"""
+    #Formula taken from AQI calculator excel sheet
+    if no2 <= 40:
+        return no2
+    elif no2 > 40 and no2 <= 80:
+        return 50 + (no2 - 40)
+    elif no2 > 80 and no2 <= 180:
+        return 100 + (no2 - 80)
+    elif no2 > 180 and no2 <= 280:
+        return 200 + (no2 - 180)
+    elif no2 > 280 and no2 <= 400:
+        return 300 + (no2 - 280) * (100 / 120)
+    elif no2 > 400:
+        return 400 + (no2 - 400) * (100 / 120)
+
 df_list = construct_initial_dataframe()
 for df in df_list:
-    df['RSPM/PM10'] = df['RSPM/PM10'].map(lambda pm_10: calculate_sub_index_pm10(pm_10))
-    df['SO2'] = df['SO2'].map(lambda so2: calculate_sub_index_so2(so2))
+    df['RSPM/PM10'] = df['RSPM/PM10'].map(lambda pm_10: int(calculate_sub_index_pm10(pm_10)))
+    df['SO2'] = df['SO2'].map(lambda so2: int(calculate_sub_index_so2(so2)))
+    df['NO2'] = df['NO2'].map(lambda no2: int(calculate_sub_index_no2(no2)))
+    
