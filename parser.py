a608b97f728be6e1045f618238e41abad37af59d
yesemsanthoshkumar <yesemsanthoshkumar@gmail.com>
2017-11-14 23:03:07 +0530

    Concatenated dataframe implementation

--- parser.py
+++ parser.py
@@ -1,22 +1,13 @@
+"""Analysing Tamilnadu air quality"""
+
 import os
 
-import numpy
+import numpy as np
 import pandas as pd
 
-
-def construct_initial_dataframe():
-    """Function to read resources and construct the initial dataframe"""
-    reqd_cols = ['Sampling Date', 'State', 'City/Town/Village/Area', 'Location of Monitoring Station', 'SO2', 'NO2', 'RSPM/PM10']
-    #Reading all files inside resources folder
-    files_list = os.listdir('resources')
-    df_list = []
-    for filex in files_list:
-        df = pd.read_csv('.\\resources\\'+filex, usecols=reqd_cols)
-        df.fillna(0, inplace=True)
-        df[['SO2', 'NO2', 'RSPM/PM10']] = df[['SO2', 'NO2', 'RSPM/PM10']].astype(numpy.int64)
-        df_list.append(df)
-    #Sending back a list of dataframes
-    return df_list
+REQ_COLUMNS = ['Sampling Date', 'State', 'City/Town/Village/Area',
+               'Location of Monitoring Station', 'SO2', 'NO2',
+               'RSPM/PM10']
 
 def calculate_sub_index_pm10(pm_10):
     """Function to calculate the sub-index of the pollutant PM10"""
@@ -37,16 +28,16 @@ def calculate_sub_index_so2(so2):
     #Formula taken from AQI calculator excel sheet
     if so2 <= 40:
         return so2*50/40
-    elif so2 > 40 and so2 <=80:
+    elif so2 > 40 and so2 <= 80:
         return 50 + (so2 - 40) * 50/40
     elif so2 > 80 and so2 <= 380:
-        return 100 + ( so2- 80) * 100/300
+        return 100 + (so2- 80) * 100/300
     elif so2 > 380 and so2 <= 380:
         return 200 + (so2 - 380) * 100/420
     elif so2 > 800 and so2 <= 1600:
         return 300 + (so2 - 800) * 100/800
     elif so2 > 1600:
-        return 400 + ( so2 - 1600) * 100/80
+        return 400 + (so2 - 1600) * 100/80
 
 def calculate_sub_index_no2(no2):
     """Function to calculate the sub-index of the pollutant no2"""
@@ -64,9 +55,20 @@ def calculate_sub_index_no2(no2):
     elif no2 > 400:
         return 400 + (no2 - 400) * (100 / 120)
 
-df_list = construct_initial_dataframe()
-for df in df_list:
-    df['RSPM/PM10'] = df['RSPM/PM10'].map(lambda pm_10: int(calculate_sub_index_pm10(pm_10)))
-    df['SO2'] = df['SO2'].map(lambda so2: int(calculate_sub_index_so2(so2)))
-    df['NO2'] = df['NO2'].map(lambda no2: int(calculate_sub_index_no2(no2)))
-    
+if __name__ == "__main__":
+    data = pd.DataFrame()
+    for data_f in os.listdir('resources'):
+        tdf = pd.read_csv(
+            os.path.join('resources', data_f),
+            usecols=REQ_COLUMNS
+        )
+        data = pd.concat([
+            data,
+            tdf
+        ])
+    data = data.reset_index(drop=True)
+    data['SO2'] = data['SO2'].apply(calculate_sub_index_so2)
+    data['NO2'] = data['NO2'].apply(calculate_sub_index_no2)
+    data['RSPM/PM10'] = data['RSPM/PM10'].apply(calculate_sub_index_pm10)
+    print(data.info())
+    print(data.head())
