9e96eaf493dd93120b5cab2049d3fd0b879d3399
Surya Raman <ayrusme@gmail.com>
2017-12-15 13:44:21 +0530

    Added bokeh dependency

--- parser.py
+++ parser.py
@@ -5,12 +5,15 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from bokeh.io import save
+from bokeh.models import BoxAnnotation
+from bokeh.plotting import figure, output_file
 
-REQ_COLUMNS = ['Sampling Date', 'State', 'City/Town/Village/Area',
+REQ_COLUMNS = ['Stn Code', 'Sampling Date', 'State', 'City/Town/Village/Area',
                'Location of Monitoring Station', 'SO2', 'NO2',
                'RSPM/PM10']
-FINAL_COLUMNS = ['Sampling Date', 'State', 'City/Town/Village/Area',
-               'Location of Monitoring Station', 'AQI']
+AGG_COLUMNS = ['Sampling Date', 'Location of Monitoring Station', 'AQI']
+FINAL_COLUMNS = ['Sampling Date', 'AQI']
 
 def calculate_sub_index_pm10(pm_10):
     """Function to calculate the sub-index of the pollutant PM10"""
@@ -69,10 +72,46 @@ if __name__ == "__main__":
             data,
             tdf
         ])
-    data = data.reset_index(drop=True)
+    data['Sampling Date'] = pd.to_datetime(data['Sampling Date'])
     data['SO2'] = data['SO2'].apply(calculate_sub_index_so2)
     data['NO2'] = data['NO2'].apply(calculate_sub_index_no2)
     data['RSPM/PM10'] = data['RSPM/PM10'].apply(calculate_sub_index_pm10)
     data['AQI'] = data[['SO2', 'NO2', 'RSPM/PM10']].max(axis=1)
-    data = data[FINAL_COLUMNS]
-    
+    data.set_index('Stn Code', inplace=True)
+    data = data[AGG_COLUMNS]
+    group_object = data.groupby(['Stn Code'])
+    key_values = group_object.groups.keys()
+    for key in key_values:
+        sub_data = group_object.get_group(key)
+        location = sub_data.iloc[0]['Location of Monitoring Station']
+
+        #Sort the df
+        sub_data.sort_values(by='Sampling Date', inplace=True)
+
+        #Create the plot
+        plot = figure(plot_width=3840, plot_height=2160, x_axis_type="datetime")
+        #Set attributes
+        plot.xaxis.axis_label = 'Date'
+        plot.yaxis.axis_label = 'Air Quality Index'
+
+        #Construct the image
+        plot.line(x=sub_data['Sampling Date'], y=sub_data['AQI'])
+
+        #Add Plot Bands
+        good = BoxAnnotation(bottom=0, top=50, fill_alpha=0.1, fill_color='#79bc6a')
+        satisfactory = BoxAnnotation(bottom=50, top=100, fill_alpha=0.1, fill_color='#bbcf4c')
+        moderately_polluted = BoxAnnotation(bottom=100, top=200, fill_alpha=0.1, fill_color='#FFCF00')
+        poor = BoxAnnotation(bottom=200, top=300, fill_alpha=0.1, fill_color='#FF9A00')
+        very_poor = BoxAnnotation(bottom=300, top=400, fill_alpha=0.1, fill_color='red')
+        severe = BoxAnnotation(bottom=400, top=500, fill_alpha=0.1, fill_color='brown')
+
+        plot.add_layout(good)
+        plot.add_layout(satisfactory)
+        plot.add_layout(moderately_polluted)
+        plot.add_layout(poor)
+        plot.add_layout(very_poor)
+        plot.add_layout(severe)
+
+        #Save the image
+        output_file(".\\images\\"  + location + ".html")
+        save(plot)
