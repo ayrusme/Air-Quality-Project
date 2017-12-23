07ce8e504af5e5391a4099a294752b3f6b5fed1d
Surya Raman <ayrusme@gmail.com>
2017-12-24 02:16:02 +0530

    Added tabs. Trying out new ways to render.

--- parser.py
+++ parser.py
@@ -1,19 +1,23 @@
 """Analysing Tamilnadu air quality"""
 
 import os
+from random import sample
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from bokeh.io import save
 from bokeh.models import BoxAnnotation
+from bokeh.models.widgets import Panel, Tabs
+from bokeh.palettes import Category20
 from bokeh.plotting import figure, output_file
 
 REQ_COLUMNS = ['Stn Code', 'Sampling Date', 'State', 'City/Town/Village/Area',
                'Location of Monitoring Station', 'SO2', 'NO2',
                'RSPM/PM10']
-AGG_COLUMNS = ['Sampling Date', 'Location of Monitoring Station', 'AQI']
 FINAL_COLUMNS = ['Sampling Date', 'AQI']
+
+COLOR_CODES = Category20[20]
 
 def calculate_sub_index_pm10(pm_10):
     """Function to calculate the sub-index of the pollutant PM10"""
@@ -61,6 +65,34 @@ def calculate_sub_index_no2(no2):
     elif no2 > 400:
         return 400 + (no2 - 400) * (100 / 120)
 
+def create_plot():
+    """Function to create a Bokeh Plot"""
+    #Create the plot
+    plot = figure(plot_width=1920, plot_height=1080, x_axis_type="datetime")
+    #Set attributes
+    plot.xaxis.axis_label = 'Date'
+    plot.yaxis.axis_label = 'Air Quality Index'
+
+    #Add Plot Bands
+    good = BoxAnnotation(bottom=0, top=50, fill_alpha=0.2, fill_color='#79bc6a')
+    satisfactory = BoxAnnotation(bottom=50, top=100, fill_alpha=0.2, fill_color='#bbcf4c')
+    moderately_polluted = BoxAnnotation(bottom=100, top=200, fill_alpha=0.2, fill_color='#FFCF00')
+    poor = BoxAnnotation(bottom=200, top=300, fill_alpha=0.2, fill_color='#FF9A00')
+    very_poor = BoxAnnotation(bottom=300, top=400, fill_alpha=0.2, fill_color='red')
+    severe = BoxAnnotation(bottom=400, top=500, fill_alpha=0.2, fill_color='brown')
+
+    plot.add_layout(good)
+    plot.add_layout(satisfactory)
+    plot.add_layout(moderately_polluted)
+    plot.add_layout(poor)
+    plot.add_layout(very_poor)
+    plot.add_layout(severe)
+
+    plot.legend.click_policy="hide"
+
+    return plot
+
+
 if __name__ == "__main__":
     data = pd.DataFrame()
     for data_f in os.listdir('resources'):
@@ -78,40 +110,29 @@ if __name__ == "__main__":
     data['RSPM/PM10'] = data['RSPM/PM10'].apply(calculate_sub_index_pm10)
     data['AQI'] = data[['SO2', 'NO2', 'RSPM/PM10']].max(axis=1)
     data.set_index('Stn Code', inplace=True)
-    data = data[AGG_COLUMNS]
-    group_object = data.groupby(['Stn Code'])
+    group_object = data.groupby(['City/Town/Village/Area'])
     key_values = group_object.groups.keys()
     for key in key_values:
         sub_data = group_object.get_group(key)
-        location = sub_data.iloc[0]['Location of Monitoring Station']
+        sub_group_object = sub_data.groupby(['Stn Code'])
+        sub_key_values = sub_group_object.groups.keys()
+        tabs = []
+        for sub_key, color in zip(sub_key_values, COLOR_CODES):    
+            location_data = sub_group_object.get_group(sub_key)
 
-        #Sort the df
-        sub_data.sort_values(by='Sampling Date', inplace=True)
+            legend = location_data.iloc[0]['Location of Monitoring Station']
+            
+            location_data['Sampling Date'] = pd.to_datetime(location_data['Sampling Date'])
+            #Sort the df
+            location_data.sort_values(by='Sampling Date', inplace=True)
 
-        #Create the plot
-        plot = figure(plot_width=3840, plot_height=2160, x_axis_type="datetime")
-        #Set attributes
-        plot.xaxis.axis_label = 'Date'
-        plot.yaxis.axis_label = 'Air Quality Index'
-
-        #Construct the image
-        plot.line(x=sub_data['Sampling Date'], y=sub_data['AQI'])
-
-        #Add Plot Bands
-        good = BoxAnnotation(bottom=0, top=50, fill_alpha=0.1, fill_color='#79bc6a')
-        satisfactory = BoxAnnotation(bottom=50, top=100, fill_alpha=0.1, fill_color='#bbcf4c')
-        moderately_polluted = BoxAnnotation(bottom=100, top=200, fill_alpha=0.1, fill_color='#FFCF00')
-        poor = BoxAnnotation(bottom=200, top=300, fill_alpha=0.1, fill_color='#FF9A00')
-        very_poor = BoxAnnotation(bottom=300, top=400, fill_alpha=0.1, fill_color='red')
-        severe = BoxAnnotation(bottom=400, top=500, fill_alpha=0.1, fill_color='brown')
-
-        plot.add_layout(good)
-        plot.add_layout(satisfactory)
-        plot.add_layout(moderately_polluted)
-        plot.add_layout(poor)
-        plot.add_layout(very_poor)
-        plot.add_layout(severe)
-
+            plot = create_plot()
+            #Construct the image
+            plot.line(x=location_data['Sampling Date'], y=location_data['AQI'], 
+                      legend=legend, line_color=color)
+            tabs.append(Panel(child=plot, title=legend))
+        
+        render_plot = Tabs(tabs=tabs)
         #Save the image
-        output_file(".\\images\\"  + location + ".html")
-        save(plot)
+        output_file(".\\images\\"  + key + ".html")
+        save(render_plot)
